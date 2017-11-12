6d1d920f3df3bff7127fbeaee6bd001c3f6da952
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2017-11-12 14:39:51 +0200

    data wrangling - data investigation

---
+++ l1-c1.py
@@ -0,0 +1,98 @@
+import unicodecsv
+from datetime import datetime as dt
+
+def read_csv(file_path):
+	with open(file_path, 'rb') as file:
+		reader = unicodecsv.DictReader(file)
+		return list(reader)
+
+def parse_date(date):
+	if date == '':
+		return None
+	else:
+		return dt.strptime(date, '%Y-%m-%d')
+
+def parse_state(state):
+	return state == 'True'
+
+def parsing(data_list):
+	for data in data_list:
+		data['join_date'] = parse_date(data['join_date'])
+		data['cancel_date'] = parse_date(data['cancel_date'])
+		data['is_udacity'] = parse_state(data['is_udacity'])
+		data['is_canceled'] = parse_state(data['is_canceled'])
+	return data_list
+
+def uniqueness_number(data_list):
+	account_key_set = set()
+	for index in range(len(data_list)):
+		account_key_set.add(data_list[index]['account_key'])
+	return account_key_set
+
+def replace_key(data_list):
+	for item in data_list:
+		value = item.pop('crt', None)
+		item.update({'account_key': value})
+	return data_list
+
+def undetected_rows(student_set, daily_engagement):
+	strange_row = []
+	for row in daily_engagement:
+		if row['account_key'] in student_set:
+			strange_row.append(row)
+	return strange_row
+
+def check_date(start_date, cancel_date):
+	return start_date != cancel_date
+
+def anssure_cancelating(not_engagement):
+	diff_row = []
+	for row in not_engagement:
+		if check_date(row['join_date'], row['cancel_date']):
+			diff_row.append(row)
+	return diff_row
+
+def remove_udacity_accounts(enrollment):
+	enrollment_student = []
+	for student in enrollment:
+		if not student['is_udacity']:
+			enrollment_student.append(student)
+	return enrollment_student
+
+# Wrangling Phase Ph.1
+enrollment = read_csv('E:\Data Analysis\c-ud170\enrollments.csv')
+daily_engagement = read_csv('E:\Data Analysis\c-ud170\daily_engagement.csv')
+project_submissions = read_csv('E:\Data Analysis\c-ud170\project_submissions.csv')
+
+# Parsing Data Ph.1
+enrollment = parsing(enrollment)
+# Investigation Phase Ph.2
+enrollment_row = len(enrollment)
+enrollment_uniqueness_student_set = uniqueness_number(enrollment)
+enrollment_uniqueness_student_number = len(enrollment_uniqueness_student_set)
+
+daily_engagement_row = len(daily_engagement)
+daily_engagement_replacing = replace_key(daily_engagement)
+daily_engagement_uniqueness_student_set = uniqueness_number(daily_engagement_replacing)
+daily_engagement_uniqueness_student_number = len(daily_engagement_uniqueness_student_set)
+
+project_submissions_row = len(project_submissions)
+project_submissions_uniqueness_student_set = uniqueness_number(project_submissions)
+project_submissions_uniqueness_student_number = len(project_submissions_uniqueness_student_set)
+
+
+print("Rows Number: Enrollment {}, Daily Engagement {}, Project Submissions {} \n\nUniqueness Student: Enrollment {}, Daily Engagement {}, Project Submissions {}"
+	.format(enrollment_row, daily_engagement_row, project_submissions_row, 
+		enrollment_uniqueness_student_number, daily_engagement_uniqueness_student_number, project_submissions_uniqueness_student_number))
+
+# Missing students Ph.2
+enrollment_student_not_engagement = undetected_rows(enrollment_uniqueness_student_set - daily_engagement_uniqueness_student_set,enrollment)
+
+#Checker investigation ansure
+enrollment_student_not_engagement_day = anssure_cancelating(enrollment_student_not_engagement)
+
+print("Student With Diff {}".format(len(enrollment_student_not_engagement_day)))
+# Detect three student ... they were a vertual student so that their data has true in is_udacity
+# remove all udacity accounts.
+enrollments_not_udacity_accounts = remove_udacity_accounts(enrollment)
+# Inverstigation is done.
