1768441770279b96c927af2da3bda3e4dbac14cc
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2017-11-19 06:27:36 -0800

    Ph3. 2 get paid student during first week

--- l1-c1.py
+++ l1-c1.py
@@ -6,11 +6,17 @@ def read_csv(file_path):
 		reader = unicodecsv.DictReader(file)
 		return list(reader)
 
-def parse_date(date):
+def parse_date_enrollment(date):
 	if date == '':
 		return None
 	else:
 		return dt.strptime(date, '%Y-%m-%d')
+
+def parse_date_engagement(date):
+	if date == '':
+		return None
+	else:
+		return dt.strptime(date, '%m/%d/%Y')
 
 def parse_num(data):
 	if data == '':
@@ -21,14 +27,18 @@ def parse_num(data):
 def parse_state(state):
 	return state == 'True'
 
-def parsing(data_list):
+def parsing_engagement(data_list):
 	for data in data_list:
-		data['join_date'] = parse_date(data['join_date'])
-		data['cancel_date'] = parse_date(data['cancel_date'])
+		data['utc_date'] = parse_date_engagement(data['utc_date'])
+	return data_list
+
+def parsing_enrollment(data_list):
+	for data in data_list:
+		data['join_date'] = parse_date_enrollment(data['join_date'])
+		data['cancel_date'] = parse_date_enrollment(data['cancel_date'])
 		data['is_udacity'] = parse_state(data['is_udacity'])
 		data['is_canceled'] = parse_state(data['is_canceled'])
 		data['days_to_cancel'] = parse_num(data['days_to_cancel'])
-		data['account_key'] = parse_num(data['account_key'])
 	return data_list
 
 def uniqueness_number(data_list):
@@ -60,12 +70,19 @@ def anssure_cancelating(not_engagement):
 			diff_row.append(row)
 	return diff_row
 
-def remove_udacity_accounts(enrollment):
-	enrollment_student = []
+def udacity_account_keys(enrollment):
+	udacity_accounts = set()
 	for student in enrollment:
-		if not student['is_udacity']:
-			enrollment_student.append(student)
-	return enrollment_student
+		if student['is_udacity']:
+			udacity_accounts.add(student['account_key'])
+	return udacity_accounts
+
+def remove_udacity_accounts(data, udacity_accounts):
+	student_data = []
+	for student in data:
+		if not student['account_key'] in udacity_accounts:
+			student_data.append(student)
+	return student_data
 
 def filter_paid_student(enrollments):
 	paid_student = dict()
@@ -77,13 +94,26 @@ def filter_paid_student(enrollments):
 				paid_student[student['account_key']] = student['join_date']
 	return paid_student
 
+def during_first_week(join_student_date, engagement_date):
+	time_delta = engagement_date - join_student_date
+	return  time_delta.days < 7 
+
+def filter_engagement_paid_first_week(paid_student, daily_engagement):
+	paid_student_first_week = []
+	for student_engag in daily_engagement:
+		account_key = student_engag['account_key']
+		if account_key in paid_student and during_first_week(paid_student[account_key], student_engag['utc_date']):
+			paid_student_first_week.append(student_engag)
+	return paid_student_first_week
+
 # Wrangling Phase Ph.1
 enrollment = read_csv('E:\Data Analysis\c-ud170\enrollments.csv')
 daily_engagement = read_csv('E:\Data Analysis\c-ud170\daily_engagement.csv')
 project_submissions = read_csv('E:\Data Analysis\c-ud170\project_submissions.csv')
 
 # Parsing Data Ph.1
-enrollment = parsing(enrollment)
+enrollment = parsing_enrollment(enrollment)
+daily_engagement = parsing_engagement(daily_engagement)
 # Investigation Phase Ph.2
 enrollment_row = len(enrollment)
 enrollment_uniqueness_student_set = uniqueness_number(enrollment)
@@ -114,13 +144,17 @@ enrollment_student_not_engagement_day = anssure_cancelating(enrollment_student_n
 print("Student With Diff {}".format(len(enrollment_student_not_engagement_day)))
 # Detect three student ... they were a vertual student so that their data has true in is_udacity
 # remove all udacity accounts.
-enrollments_not_udacity_accounts = remove_udacity_accounts(enrollment)
+udacity_accounts = udacity_account_keys(enrollment)
+enrollments_not_udacity_accounts = remove_udacity_accounts(enrollment, udacity_accounts)
 # Inverstigation is done.
 # Result Enrollments List Without Virtual Udacity Accounts, Detect There are student unenrolled in course 
 # In the same day.
 
 # Exploration Phase 3.
 enrollments_paid_student = filter_paid_student(enrollments_not_udacity_accounts)
-print(len(enrollments_paid_student))
-# Ph3.2
+print("paid students 'enrollment' : {}".format(len(enrollments_paid_student)))
+# Ph3.2 filter paid students during their first week
+engagments_not_udacity_accounts = remove_udacity_accounts(daily_engagement, udacity_accounts)
+paid_student_engagement_first_week = filter_engagement_paid_first_week(enrollments_paid_student, engagments_not_udacity_accounts)
 
+print("paid student during first week {}".format(len(paid_student_engagement_first_week)))
